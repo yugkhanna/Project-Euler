bd5335c12b3c28135d5ac99da0f9e9d58156daac
Yug Khanna <yugkhanna1@gmail.com>
2018-07-24 20:04:27 +0530

    q2 added

---
+++ problem2.py
@@ -0,0 +1,9 @@
+max_val = 0
+for i in range(999,99,-1):
+    for j in range(999,99,-1):
+        ij = i*j
+        if str(ij) == str(ij)[::-1]:
+            if ij>max_val:
+                max_val = ij
+
+print (max_val)
